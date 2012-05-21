4d92da6409647d4396ffa18ab524d5d216812020
Ilmari Vacklin <ilmari.vacklin@eficode.com>
2012-05-21 16:59:30 +0300

    Add CSS.
    - http://twitter.github.com/bootstrap gives us the base styles.
    - Custom styles via cssgen.
    
    We also defined our own text-field to use instead of Hiccup's, because
    we wanted to wrap the field in a div to get Bootstrap's form styles to
    work, and we wanted to define a placeholder.

--- src/blorg/core.clj
+++ src/blorg/core.clj
@@ -1,30 +1,30 @@
 (ns blorg.core
   (:use noir.core)
-  (:use hiccup.form)
+  (:require [hiccup.form :as form])
   (:require [noir.server :as server]
             [noir.response :as response]
-            [hiccup.page :as page]))
+            [hiccup.page :as page]
+            [cssgen :as css]))
 
 (def posts (atom [{:title "foo" :content "bar"}
                   {:title "quux" :content "ref ref"}]))
 
+(defn text-field [name placeholder]
+  [:div {:class "text-field"}
+   [:input {:type "textfield" :placeholder placeholder :name name}]])
+
 (defn add-form []
   [:section
    [:h2 "Add post"]
-   (form-to [:post "/"]
-            (label "title" "Title")
-            [:br]
-            (text-field "title")
-            [:br]
-            (label "content" "Content")
-            [:br]
-            (text-area "content")
-            [:br]
-            (submit-button "Add"))])
+   (form/form-to [:post "/"]
+            [:div {:class "text-field"}
+             (text-field "Title" "title")]
+            [:div {:class "text-area"}
+             (form/text-area {:placeholder "Content"} "content")]
+            [:button {:type "submit" :class "btn"} "Add"])])
 
 (defn list-posts []
   [:section
-   [:h2 "Posts"]
    (for [post @posts]
     [:section
      [:h3 (:title post)]
@@ -32,13 +32,31 @@
 
 (defpage "/" []
   (page/html5
-   [:section
-    (list-posts)
-    (add-form)]))
+   [:head
+    (page/include-css "css/base")
+    (page/include-css "css/bootstrap")]
+   [:body
+    [:div {:class "container"}
+     [:div {:class "row"}
+      [:div {:class "span12"}
+       [:header {:class "page-header"}
+        [:h1 "Blorg"]]
+       (list-posts)
+       (add-form)]]]]))
 
 (defpage [:post "/"] {:keys [title content]}
   (swap! posts #(conj % {:title title :content content}))
   (response/redirect "/"))
+
+(defpage "/css/bootstrap" []
+  (response/content-type
+   "text/css"
+   (slurp "bootstrap.css")))
+
+(defpage "/css/base" []
+  (response/content-type
+   "text/css" (css/css [:html :font-family "sans-serif"])
+))
 
 (defn -main [& args]
   (println "> blorg blog blorg")
