d4570297c5ffe9c22cded9f035d3615b94e9c549
Ilmari Vacklin <ilmari.vacklin@eficode.com>
2012-05-21 16:26:41 +0300

    Allow adding new posts.
    This works by storing the vector of posts in an atom. See
    http://clojure.org/atoms for more information about them; basically,
    they manage shared, synchronous state with minimal fuss.

--- src/blorg/core.clj
+++ src/blorg/core.clj
@@ -1,17 +1,44 @@
 (ns blorg.core
   (:use noir.core)
+  (:use hiccup.form)
   (:require [noir.server :as server]
+            [noir.response :as response]
             [hiccup.page :as page]))
 
-(def *posts* [{:title "foo" :content "bar"}
-              {:title "quux" :content "ref ref"}])
+(def posts (atom [{:title "foo" :content "bar"}
+                  {:title "quux" :content "ref ref"}]))
+
+(defn add-form []
+  [:section
+   [:h2 "Add post"]
+   (form-to [:post "/"]
+            (label "title" "Title")
+            [:br]
+            (text-field "title")
+            [:br]
+            (label "content" "Content")
+            [:br]
+            (text-area "content")
+            [:br]
+            (submit-button "Add"))])
+
+(defn list-posts []
+  [:section
+   [:h2 "Posts"]
+   (for [post @posts]
+    [:section
+     [:h3 (:title post)]
+     [:p (:content post)]])])
 
 (defpage "/" []
   (page/html5
-   (for [post *posts*]
-     [:section
-      [:h2 (:title post)]
-      [:p (:content post)]])))
+   [:section
+    (list-posts)
+    (add-form)]))
+
+(defpage [:post "/"] {:keys [title content]}
+  (swap! posts #(conj % {:title title :content content}))
+  (response/redirect "/"))
 
 (defn -main [& args]
   (println "> blorg blog blorg")
