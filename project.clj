da7261b5cad703f02a715ed29c68a8b3b6dfefb0
Ilmari Vacklin <ilmari.vacklin@eficode.com>
2012-05-22 13:02:11 +0300

    Persist posts to posts.json

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject blorg "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [noir "1.3.0-beta7"]
-                 [cssgen "0.3.0-alpha1"]]
+                 [cssgen "0.3.0-alpha1"]
+                 [cheshire "4.0.0"]]
   :main blorg.core)
