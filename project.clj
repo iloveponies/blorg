bcfdccc3b4dfc3162350da9a5f335f83ccd520ed
Ilmari Vacklin <ilmari.vacklin@eficode.com>
2012-05-24 12:46:23 +0300

    Add main namespace

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject blorg "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [noir "1.3.0-beta7"]])
+                 [noir "1.3.0-beta7"]]
+  :main blorg.core)
